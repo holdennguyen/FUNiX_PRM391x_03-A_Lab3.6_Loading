039fd26e474e61f055a0ccff8f56f9f0558a9cae
Hung. Minh Nguyen <89327966+holdennguyen@users.noreply.github.com>
2022-06-08 20:09:09 +0700

    initialization

---
+++ app/src/main/java/com/example/animalsound/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.animalsound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m001_act_splash);
+    }
+}
